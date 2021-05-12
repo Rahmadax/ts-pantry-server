7eca4ebeca01cf94c5eb83ebaea157e0ec96c9c3
Tom Greasley <tom.greasley@depop.com>
2021-05-12 16:13:45 +0100

    Initial Commit

---
+++ src/main/kotlin/com/depop/cx/drc/DrcApplication.kt
@@ -0,0 +1,11 @@
+package com.depop.cx.drc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DrcApplication
+
+fun main(args: Array<String>) {
+    runApplication<DrcApplication>(*args)
+}
---
+++ src/test/kotlin/com/depop/cx/drc/DrcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.depop.cx.drc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DrcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
