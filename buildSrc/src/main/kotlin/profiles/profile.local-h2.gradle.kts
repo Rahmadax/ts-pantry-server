c3dacce8e2ae8f1b55eeb01628828396df38ae60
Tom Greasley <tom.greasley@depop.com>
2021-05-17 18:44:24 +0100

    CXPROD-546 Create Spring Boot BPM Application
    Set spring profiles bases on gradle build profiles

--- buildSrc/src/main/kotlin/profiles/profile.local-h2.gradle.kts
+++ buildSrc/src/main/kotlin/profiles/profile.local-h2.gradle.kts
@@ -1,7 +1,13 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 plugins {
     id("drc-workflow.kotlin-conventions") apply false
 }
 
 dependencies {
     runtimeOnly("com.h2database:h2")
+}
+
+tasks.named<BootRun>("bootRun") {
+    args("--spring.profiles.active=local,h2")
 }
--- buildSrc/src/main/kotlin/profiles/profile.local-postgres.gradle.kts
+++ buildSrc/src/main/kotlin/profiles/profile.local-postgres.gradle.kts
@@ -1,7 +1,13 @@
+import org.springframework.boot.gradle.tasks.run.BootRun
+
 plugins {
     id("drc-workflow.kotlin-conventions") apply false
 }
 
 dependencies {
     runtimeOnly("org.postgresql:postgresql")
+}
+
+tasks.named<BootRun>("bootRun") {
+    args("--spring.profiles.active=local,postgres")
 }
