ca8b18d13f1ead61244400aa0e8a96cefb1cd89a
Tom Greasley <tom.greasley@depop.com>
2021-07-07 15:35:23 +0100

    CXPROD-616 Prepare BPM application for staging deployment
    Changes from code-review: Removing commented out code.

--- dispute-workflow/src/main/java/com/depop/cx/drc/workflow/DrcWorkflowServer.java
+++ dispute-workflow/src/main/java/com/depop/cx/drc/workflow/DrcWorkflowServer.java
@@ -1,21 +1,10 @@
 package com.depop.cx.drc.workflow;
 
-import org.camunda.bpm.engine.RuntimeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.Resource;
-
 @SpringBootApplication
 public class DrcWorkflowServer {
-
-    @Resource
-    private RuntimeService runtimeService;
-
-//    @Scheduled(fixedRate = 5000)
-//    public void startProcess() {
-//        runtimeService.startProcessInstanceByKey("hello_world_process");
-//    }
 
     public static void main(final String[] args) {
         SpringApplication.run(DrcWorkflowServer.class, args);
--- dispute-workflow/src/main/java/com/depop/cx/drc/workflow/config/ServerConfiguration.java
+++ dispute-workflow/src/main/java/com/depop/cx/drc/workflow/config/ServerConfiguration.java
@@ -1,12 +1,10 @@
 package com.depop.cx.drc.workflow.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@EnableScheduling
 public class ServerConfiguration {
     // Empty
 }
