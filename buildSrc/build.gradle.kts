9bc631a28638473dd44bd003d6b2cfbd68a159fe
Tom Greasley <965075+schlafsack@users.noreply.github.com>
2021-07-09 14:02:54 +0100

    CXPROD-616 Prepare BPM application for staging deployment
    Updated spring boot version from 2.5.0 -> 2.5.1
    Fixed a bug in the EngineMetricsRecorder - it was not adding a prefix to the meter names.
    Set the path to the camunda rest api to /camunda/rest

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     implementation(kotlin(module = "gradle-plugin", version = "1.4.31"))
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.4.31")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.0")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.1")
 }
 
