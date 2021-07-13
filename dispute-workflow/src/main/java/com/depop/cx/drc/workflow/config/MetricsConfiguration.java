b752d641a7e127d77698edfc70c20853a2b5129f
sorin-vava-depop <sorin.vava@depop.com>
2021-07-13 11:10:55 +0300

    Added tags & Refactored jvm metrics
    - Added tags to comply Depop tagging proposals
    - Refactored jvm metrics in a new bean

--- dispute-workflow/src/main/java/com/depop/cx/drc/workflow/config/MetricsConfiguration.java
+++ dispute-workflow/src/main/java/com/depop/cx/drc/workflow/config/MetricsConfiguration.java
@@ -1,6 +1,7 @@
 package com.depop.cx.drc.workflow.config;
 
 import com.depop.cx.drc.workflow.metrics.engine.EngineCustomMetricsPlugin;
+import com.depop.cx.drc.workflow.metrics.engine.EngineJavaMetricsPlugin;
 import com.depop.cx.drc.workflow.metrics.engine.EngineMetricsPlugin;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
@@ -22,13 +23,14 @@ public class MetricsConfiguration {
     @Bean
     @ConditionalOnMissingBean(CamundaMetricsConfiguration.class)
     public CamundaMetricsConfiguration camundaMetricsConfiguration(final MeterRegistry meterRegistry) {
-//        meterRegistry.config().commonTags("host_tag", "dispute-workflow");
+        meterRegistry.config().commonTags("service_domain", "api");
+        meterRegistry.config().commonTags("service_group", "bpmn");
+        meterRegistry.config().commonTags("service_name", "dispute-workflow");
+        meterRegistry.config().commonTags("service_role", "bpmn");
 
-        new ClassLoaderMetrics().bindTo(meterRegistry);
-        new JvmMemoryMetrics().bindTo(meterRegistry);
-        new JvmGcMetrics().bindTo(meterRegistry);
-        new ProcessorMetrics().bindTo(meterRegistry);
-        new JvmThreadMetrics().bindTo(meterRegistry);
+        //ex:client/controller/service/dao/listener
+        meterRegistry.config().commonTags("app.layer", "service");
+        meterRegistry.config().commonTags("class.function", "MetricsConfiguration.camundaMetricsConfiguration");
 
         return new EngineMetricsPlugin(meterRegistry);
     }
@@ -36,7 +38,31 @@ public class MetricsConfiguration {
     @Bean
     @ConditionalOnMissingBean(EngineCustomMetricsPlugin.class)
     public EngineCustomMetricsPlugin camundaCustomMetricsConfiguration(final MeterRegistry meterRegistry, final ProcessEngine processEngine) {
+        meterRegistry.config().commonTags("service_domain", "api");
+        meterRegistry.config().commonTags("service_group", "bpmn");
+        meterRegistry.config().commonTags("service_name", "dispute-workflow");
+        meterRegistry.config().commonTags("service_role", "bpmn");
+
+        //ex:client/controller/service/dao/listener
+        meterRegistry.config().commonTags("app.layer", "service");
+        meterRegistry.config().commonTags("class.function", "MetricsConfiguration.camundaCustomMetricsConfiguration");
+
         return new EngineCustomMetricsPlugin(meterRegistry, processEngine);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(EngineJavaMetricsPlugin.class)
+    public EngineJavaMetricsPlugin javaMetricsConfiguration(final MeterRegistry meterRegistry, final ProcessEngine processEngine) {
+        meterRegistry.config().commonTags("service_domain", "api");
+        meterRegistry.config().commonTags("service_group", "bpmn");
+        meterRegistry.config().commonTags("service_name", "dispute-workflow");
+        meterRegistry.config().commonTags("service_role", "bpmn");
+
+        //ex:client/controller/service/dao/listener
+        meterRegistry.config().commonTags("app.layer", "service");
+        meterRegistry.config().commonTags("class.function", "MetricsConfiguration.javaMetricsConfiguration");
+
+        return new EngineJavaMetricsPlugin(meterRegistry, processEngine);
     }
 
 }
