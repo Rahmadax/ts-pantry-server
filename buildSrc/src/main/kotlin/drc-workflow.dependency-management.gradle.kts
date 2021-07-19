bd88fe8153e7ef1ed862d48789f18b94991fbace
Tom Greasley <tom.greasley@depop.com>
2021-07-19 09:35:16 +0100

    CXPROD-560 Add BPM Engine Support
    Set default serialization to JSON
    Added camunda SPIN depependencies for JSON support
    Disabled Standalone tasks and CMMN
    Using camunda BOM rather than individual dep version mgmt.

--- buildSrc/src/main/kotlin/drc-workflow.dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/drc-workflow.dependency-management.gradle.kts
@@ -6,11 +6,13 @@ plugins {
 // NOTE: Spring boot dependency versions are managed by the spring boot BOM
 // please see drc-workflow.spring-boot-conventions.gradle.kts
 dependencyManagement {
-    dependencies {
 
-        // Camunda dependencies
-        dependency("org.camunda.bpm.springboot:camunda-bpm-spring-boot-starter:7.15.0")
-        dependency("org.camunda.bpm.springboot:camunda-bpm-spring-boot-starter-rest:7.15.0")
+    // Camunda dependencies
+    imports {
+        mavenBom("org.camunda.bpm:camunda-bom:7.15.0")
+    }
+
+    dependencies {
 
         // Opentracing dependencies
         dependency("io.opentracing.contrib:opentracing-spring-cloud-starter:0.5.9")
