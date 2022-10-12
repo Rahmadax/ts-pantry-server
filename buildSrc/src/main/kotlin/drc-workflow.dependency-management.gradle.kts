f29ae8cd9b71f7a9c6a7e570e1d8d5a9826cad8b
Damien Bailly <54623050+damienbailly@users.noreply.github.com>
2022-10-12 10:31:00 +0100

    Fix Snyk vulnerabilities (#52)
    * Fix Snyk vulns
    
    * dd-trace-api 0.110.0
    
    * okhttp3

--- buildSrc/src/main/kotlin/drc-workflow.dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/drc-workflow.dependency-management.gradle.kts
@@ -43,8 +43,8 @@ dependencyManagement {
         dependency("io.opentracing:opentracing-mock:0.33.0")
 
         // Datadog dependencies
-        dependency("com.datadoghq:dd-trace-api:0.91.0")
-        dependency("com.datadoghq:dd-trace-ot:0.91.0")
+        dependency("com.datadoghq:dd-trace-api:0.110.0")
+        dependency("com.datadoghq:dd-trace-ot:0.110.0")
 
         // General dependencies
         dependency("io.github.microutils:kotlin-logging-jvm:2.0.11")
@@ -52,8 +52,16 @@ dependencyManagement {
         // Upgrading version of jnr-posix due to snyk DOS vulnerability report.
         dependency("com.github.jnr:jnr-posix:3.1.8")
 
+        // Upgrading version of snakeyaml due to snyk DOS vulnerability report.
+        dependency("org.yaml:snakeyaml:1.31")
+
         // SNYK-JAVA-COMH2DATABASE-2331071 | SNYK-JAVA-COMH2DATABASE-1769238
-        dependency("com.h2database:h2:2.1.210")
+        dependency("com.h2database:h2:2.1.214")
+
+        // SNYK-JAVA-COMSQUAREUPOKHTTP3-2958044
+        dependency("com.squareup.okhttp3:okhttp:4.9.2")
+
+
 
     }
 
