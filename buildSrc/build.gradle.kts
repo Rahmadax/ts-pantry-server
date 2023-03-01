fdd1131abd0dfbd85f8e5d9111addebd2b3e397b
Damien Bailly <damien.bailly@depop.com>
2023-03-01 16:33:24 +0000

    Add snyk ignore

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation(kotlin(module = "gradle-plugin", version = "1.6.10"))
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.6.10")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.9")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.6.14")
     implementation("io.snyk.gradle.plugin.snykplugin:io.snyk.gradle.plugin.snykplugin.gradle.plugin:0.4")
 }
 
