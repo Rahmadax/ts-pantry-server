7947d102fcd03f4676ae8c0c84c8894be050d707
Damien Bailly <damien.bailly@depop.com>
2023-03-24 09:36:16 +0000

    json-smart 2.4.9

--- buildSrc/src/main/kotlin/drc-workflow.dependency-management.gradle.kts
+++ buildSrc/src/main/kotlin/drc-workflow.dependency-management.gradle.kts
@@ -66,6 +66,9 @@ dependencyManagement {
         // SNYK-JAVA-COMMONSFILEUPLOAD-3326457
         dependency("commons-fileupload:commons-fileupload:1.5")
 
+        // SNYK-JAVA-NETMINIDEV-3369748
+        dependency("net.minidev:json-smart:2.4.9")
+
     }
 
 }
