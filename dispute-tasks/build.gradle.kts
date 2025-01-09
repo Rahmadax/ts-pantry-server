f8671c9a7f6ceddf5ccc36c82323e62d674be8ad
Ollie Rahman <93202896+OllieRahmanDepop@users.noreply.github.com>
2025-01-09 10:13:02 +0000

    Revert "CXE 100 - Link picture task"

--- dispute-tasks/build.gradle.kts
+++ dispute-tasks/build.gradle.kts
@@ -23,9 +23,6 @@ dependencies {
     implementation("org.camunda.bpm:camunda-engine")
     implementation("org.camunda.spin:camunda-spin-core")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-
 }
 
 configurations {
