cba1cf9a9225588658a0cb8b41eb401fd6ed61ae
Martin Carolan <martin.carolan@depop.com>
2025-05-21 11:31:10 +0100

    Try again

--- dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/tasks/timer/AbstractTimerDelegate.kt
+++ dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/tasks/timer/AbstractTimerDelegate.kt
@@ -1,10 +1,12 @@
 package com.depop.cx.drc.workflow.tasks.timer
 
 import com.depop.cx.drc.workflow.AbstractDrcDelegate
+import com.depop.cx.drc.workflow.TaskErrorCode
 import com.depop.cx.drc.workflow.getStringVariableOrNull
 import org.camunda.bpm.engine.delegate.BpmnError
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.impl.persistence.entity.TimerEntity
+import org.camunda.bpm.model.bpmn.Bpmn
 
 private const val TIMER_ID_PARAM = "timer_id"
 
@@ -13,7 +15,7 @@ abstract class AbstractTimerDelegate : AbstractDrcDelegate() {
     override fun doExecute(execution: DelegateExecution) {
         val timerId = execution.getStringVariableOrNull(TIMER_ID_PARAM)
         if (timerId == null) {
-            throw BpmnError("variable $TIMER_ID_PARAM is not set")
+            throw BpmnError(TaskErrorCode.FAILURE.code, "variable $TIMER_ID_PARAM is not set")
         }
 
         val managementService = execution.processEngineServices.managementService
@@ -27,7 +29,7 @@ abstract class AbstractTimerDelegate : AbstractDrcDelegate() {
 
         when (timers.size) {
             1 -> doTimerExecute(execution, timers.single())
-            else -> throw IllegalStateException("${timers.size} timers found with id $timerId")
+            else -> throw BpmnError(TaskErrorCode.FAILURE.code, "${timers.size} timers found with id $timerId")
         }
     }
 
