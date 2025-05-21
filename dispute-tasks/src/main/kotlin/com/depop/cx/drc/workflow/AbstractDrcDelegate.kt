d534f26ad310b9c32ce57dd4e122e5a168d700f2
Martin Carolan <martin.carolan@depop.com>
2025-05-21 16:33:10 +0100

    try a different incident type

--- dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/AbstractDrcDelegate.kt
+++ dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/AbstractDrcDelegate.kt
@@ -25,7 +25,7 @@ abstract class AbstractDrcDelegate : JavaDelegate {
 
     private fun handleException(exception: Throwable, delegateExecution: DelegateExecution?) {
         logger.error(exception) { "Failed to execute task ${delegateExecution?.id ?: "<unknown>"}" }
-        delegateExecution?.createIncident("failedJob", exception.message)
+        delegateExecution?.createIncident("foobar", exception.message)
     }
 
     protected fun validate(key: String, value: Any?) {
