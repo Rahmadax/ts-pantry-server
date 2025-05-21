94acbf5d8fe26409a603427107a2ff28b9468780
Martin Carolan <martin.carolan@depop.com>
2025-05-21 09:19:01 +0100

    CXE-XXX - AbstractTimerDelegate tweaks
     # What
    * Rename input variable to `timer_id`
    * Fail if we don't find exactly one timer
    
     # Why
    * We initially assumed the delegate wanted the timer's name as input
    * We should be loud if we don't find a timer as it would be a long
      feedback cycle to find out otherwise

--- dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/tasks/timer/AbstractTimerDelegate.kt
+++ dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/tasks/timer/AbstractTimerDelegate.kt
@@ -5,22 +5,28 @@ import com.depop.cx.drc.workflow.getStringVariableOrNull
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.impl.persistence.entity.TimerEntity
 
-private const val TIMER_KEY_PARAM = "timer"
+private const val TIMER_ID_PARAM = "timer_id"
 
 abstract class AbstractTimerDelegate : AbstractDrcDelegate() {
 
     override fun doExecute(execution: DelegateExecution) {
-        val timerName = execution.getStringVariableOrNull(TIMER_KEY_PARAM)
-        if(timerName != null) {
-            val managementService = execution.processEngineServices.managementService
-            managementService
-                .createJobQuery()
-                .processInstanceId(execution.processInstanceId)
-                .timers()
-                .list()
-                .filterIsInstance<TimerEntity>()
-                .filter { timerName.equals(it.jobHandlerConfiguration.toCanonicalString()) }
-                .forEach { doTimerExecute(execution, it) }
+        val timerId = execution.getStringVariableOrNull(TIMER_ID_PARAM)
+        if (timerId == null) {
+            throw IllegalArgumentException("variable $TIMER_ID_PARAM is not set")
+        }
+
+        val managementService = execution.processEngineServices.managementService
+        val timers = managementService
+            .createJobQuery()
+            .processInstanceId(execution.processInstanceId)
+            .timers()
+            .list()
+            .filterIsInstance<TimerEntity>()
+            .filter { timerId.equals(it.jobHandlerConfiguration.toCanonicalString()) }
+
+        when (timers.size) {
+            1 -> doTimerExecute(execution, timers.single())
+            else -> throw IllegalStateException("${timers.size} timers found with id $timerId")
         }
     }
 
