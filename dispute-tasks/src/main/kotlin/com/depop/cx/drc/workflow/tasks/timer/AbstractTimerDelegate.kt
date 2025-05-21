47e2ccd9af3c64f22b19fb4e3bc8af8e8f8455e6
Martin Carolan <martin.carolan@depop.com>
2025-05-21 18:05:23 +0100

    Remove debugging

--- dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/tasks/timer/AbstractTimerDelegate.kt
+++ dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/tasks/timer/AbstractTimerDelegate.kt
@@ -2,15 +2,12 @@ package com.depop.cx.drc.workflow.tasks.timer
 
 import com.depop.cx.drc.workflow.AbstractDrcDelegate
 import com.depop.cx.drc.workflow.getStringVariableOrNull
-import io.github.oshai.kotlinlogging.KotlinLogging
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.impl.persistence.entity.TimerEntity
 
 private const val TIMER_ID_PARAM = "timer_id"
 
 abstract class AbstractTimerDelegate : AbstractDrcDelegate() {
-
-    private val logger = KotlinLogging.logger {}
 
     override fun doExecute(execution: DelegateExecution) {
         val timerId = execution.getStringVariableOrNull(TIMER_ID_PARAM)
@@ -20,19 +17,14 @@ abstract class AbstractTimerDelegate : AbstractDrcDelegate() {
 
         val managementService = execution.processEngineServices.managementService
 
-        val rawTimers = managementService.createJobQuery().processInstanceId(execution.processInstanceId).timers().list().filterIsInstance<TimerEntity>()
-        rawTimers.forEach {
-            val configStr = it.jobHandlerConfiguration?.toCanonicalString()
-            logger.info { "banana Raw timer: id=${it.id}, activityId=$configStr" }
-        }
+        val rawTimers =
+            managementService.createJobQuery().processInstanceId(execution.processInstanceId).timers().list()
+                .filterIsInstance<TimerEntity>()
 
         val filteredTimers = rawTimers.filter {
             val configStr = it.jobHandlerConfiguration?.toCanonicalString()
-            logger.info { "banana Comparing '$timerId' to '$configStr'" }
             timerId == configStr
         }
-
-        logger.info { "banana ${filteredTimers.size} timers found" }
 
         when (filteredTimers.size) {
             1 -> doTimerExecute(execution, filteredTimers.single())
