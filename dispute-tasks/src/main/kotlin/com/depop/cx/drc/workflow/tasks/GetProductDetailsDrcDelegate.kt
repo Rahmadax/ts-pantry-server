fabf55f89bd6e950bd2c9bf3ba611e0b1326258d
Ollie Rahman <93202896+OllieRahmanDepop@users.noreply.github.com>
2025-09-04 10:22:12 +0100

    CXE 630 - Refactored get product details delegate (#197)
    * CXE 630 - Refactored get product details delegate
    
    * CXE 630 - Refactored to simplify the flow
    
    * CXE 630 - Added final log

--- dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/tasks/GetProductDetailsDrcDelegate.kt
+++ dispute-tasks/src/main/kotlin/com/depop/cx/drc/workflow/tasks/GetProductDetailsDrcDelegate.kt
@@ -4,73 +4,61 @@ import com.depop.cx.drc.workflow.AbstractDrcDelegate
 import com.depop.cx.drc.workflow.TaskErrorCode
 import com.depop.cx.drc.workflow.client.CheckoutClient
 import com.depop.cx.drc.workflow.client.ProductClient
-import com.depop.cx.drc.workflow.client.Receipt
 import com.depop.cx.drc.workflow.getLongVariableOrNull
 import com.depop.cx.drc.workflow.getUUIDVariableOrNull
 import com.depop.cx.drc.workflow.metrics.TaskMetrics
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.camunda.bpm.engine.delegate.BpmnError
 import org.camunda.bpm.engine.delegate.DelegateExecution
-import kotlin.math.log
 
 private const val RECEIPT_ID = "receipt_id"
 private const val PRODUCT_ID = "product_id"
 private const val IS_PRODUCT_BANNED = "is_product_banned"
+private const val IS_BUNDLE = "is_bundle"
 private const val DISPUTE_ID_PROPERTY = "dispute_id"
 
 class GetProductDetailsDrcDelegate(
     private val productClient: ProductClient,
     private val checkoutClient: CheckoutClient,
-    private val taskMetrics: TaskMetrics
+    taskMetrics: TaskMetrics
 ) : AbstractDrcDelegate(taskMetrics) {
 
     private val logger = KotlinLogging.logger {}
 
     override fun doExecute(execution: DelegateExecution) {
         val disputeId = execution.getUUIDVariableOrNull(DISPUTE_ID_PROPERTY)
+            ?: logAndThrowError("Could not find disputeId for execution: ${execution.id}")
+
         val receiptId = execution.getLongVariableOrNull(RECEIPT_ID)
-            ?: run {
-                logger.error { "Could not find receiptId id for dispute $disputeId" }
-                throw BpmnError(
-                    TaskErrorCode.FAILURE.code,
-                    "Could not find receiptId id for dispute $disputeId"
-                )
-            }
+            ?: logAndThrowError("Could not find receiptId for dispute: $disputeId")
 
         val receipt = checkoutClient.getReceipt(receiptId)
             .block()
-            ?: run {
-                logger.error { "Could not find receipt: $receiptId for dispute $disputeId" }
-                throw BpmnError(
-                    TaskErrorCode.FAILURE.code,
-                    "Could not find receipt: $receiptId for dispute $disputeId"
-                )
-            }
+            ?: logAndThrowError("Could not find receipt: $receiptId for dispute: $disputeId")
 
-        val firstProductId = getFirstProductId(receipt, execution.id)
-            ?: run {
-                logger.error { "Receipt ${receipt.id} has no product lines for dispute $disputeId" }
-                throw BpmnError(
-                    TaskErrorCode.FAILURE.code,
-                    "Receipt ${receipt.id} has no product lines for dispute $disputeId"
-                )
-            }
+        val firstProductId = receipt.lines.firstOrNull()?.productId
+            ?: logAndThrowError("No product found on receipt: $receiptId for dispute: $disputeId")
 
         val product = productClient.getProduct(firstProductId)
             .block()
-            ?: run {
-                logger.error { "Unable to find product $firstProductId on dispute $disputeId" }
-                throw BpmnError(
-                    TaskErrorCode.FAILURE.code,
-                    "Could not find product $firstProductId for dispute $disputeId"
-                )
-            }
+            ?: logAndThrowError("Unable to find product: $firstProductId on dispute: $disputeId")
 
-        execution.setVariableLocal(
-            IS_PRODUCT_BANNED,
-            ProductStatus.fromValue(product.status) == ProductStatus.BANNED
-        )
+        val isBundle = receipt.lines.size > 1
+        if (isBundle) {
+            logger.warn { "Receipt: $receiptId for dispute: $disputeId contains multiple products (${receipt.lines.size}). Falling back to first item" }
+        }
+
         execution.setVariableLocal(PRODUCT_ID, firstProductId)
+        execution.setVariableLocal(IS_PRODUCT_BANNED, ProductStatus.fromValue(product.status) == ProductStatus.BANNED)
+        execution.setVariableLocal(IS_BUNDLE, isBundle)
+    }
+
+    fun logAndThrowError(text: String): Nothing {
+        logger.error { text }
+        throw BpmnError(
+            TaskErrorCode.FAILURE.code,
+            text
+        )
     }
 
     enum class ProductStatus {
@@ -80,17 +68,11 @@ class GetProductDetailsDrcDelegate(
             fun fromValue(value: String): ProductStatus {
                 return when (value.uppercase()) {
                     "B" -> BANNED
+                    "C" -> BANNED
                     else -> OTHER
                 }
             }
         }
     }
 
-    private fun getFirstProductId(receipt: Receipt, executionId: String): Long? {
-        if (receipt.lines.size > 1) {
-            logger.error { "Receipt ${receipt.id} has multiple product lines (${receipt.lines.size}). Cannot process for $executionId. Falling back to first item" }
-        }
-
-        return receipt.lines.firstOrNull()?.productId
-    }
 }
